7847deedcbb19963d7c408484eea93223413e9c8
Ricardo Rech <ricardorech@hotmail.com>
2022-02-13 21:28:17 -0300

    Add translator

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from greets import greetings
+from translate import Translator
+
+tranlator = Translator(to_lang='pt')
+
 
 for g in greetings:
-    print(g.title())
+    print(tranlator.translate(g).title())
 
